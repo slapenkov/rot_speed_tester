81d86958b978309f080de876bb46aaacc43020f4
Sergey Lapenkov <sergey.lapenkov@skyrora.com>
2019-06-25 01:23:37 +0300

    Add encoder support

--- rot_speed_tester.ino
+++ rot_speed_tester.ino
@@ -15,18 +15,50 @@
    ends to +5V and ground
    wiper to LCD VO pin
 
+   //Encoder
+   PB13 - PinA - DT
+   PB14 - PinB - CLK - or flipped, don't care
+   PB15 -pushbutton
+
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
+#include <RotaryEncoder.h>
+
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = PB11, en = PB10, d4 = PB0, d5 = PB1, d6 = PC14, d7 = PC15;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+
+#define PIN_A            PB14  //ky-040 clk pin, add 100nF/0.1uF capacitors between pin & ground!!!
+#define PIN_B            PB13  //ky-040 dt  pin, add 100nF/0.1uF capacitors between pin & ground!!!
+#define BUTTON           PB15  //ky-040 sw  pin, add 100nF/0.1uF capacitors between pin & ground!!!
+
+uint16_t buttonCounter = 0;
+
+RotaryEncoder     encoder(PIN_A, PIN_B, BUTTON);
+
+void encoderISR()
+{
+  encoder.readAB();
+}
+
+void encoderButtonISR()
+{
+  encoder.readPushButton();
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
+  encoder.begin();                                                           //set encoders pins as input & enable built-in pullup resistors
+
+  attachInterrupt(digitalPinToInterrupt(PIN_A),  encoderISR,       CHANGE);  //call encoderISR()    every high->low or low->high changes
+  attachInterrupt(digitalPinToInterrupt(BUTTON), encoderButtonISR, FALLING); //call pushButtonISR() every high->low              changes
+
+
   // initialize digital pin PB1 as an output.
   pinMode(PC13, OUTPUT);
 
@@ -42,9 +74,11 @@ void loop() {
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  lcd.print(encoder.getPosition());
+  lcd.setCursor(10, 1);
+  lcd.print(buttonCounter++);
   digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);              // wait for a second
+  delay(1000);              // wait for a second
   digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
   delay(100);              // wait for a second
 }
