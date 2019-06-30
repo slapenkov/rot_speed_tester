71a61973ee6ffcf55f5d0d291e38665a49546a30
Sergey Lapenkov <sergey.lapenkov@skyrora.com>
2019-06-30 20:43:39 +0300

    Implement timers setup template functions and variables

--- rot_speed_tester.ino
+++ rot_speed_tester.ino
@@ -47,7 +47,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //LCD instance
 #define BASE_PERIOD 40000
 
 uint16_t buttonCounter = 0;
-int val=0;
+int val = 0;
 
 RotaryEncoderAdvanced<float> encoder(PIN_A, PIN_B, BUTTON, 1.0, 1.0, 300.0);                         //0.1 step per click, minimum value 0, maximum value 3.3
 
@@ -55,6 +55,8 @@ HardwareTimer pwmtimer1(1);
 HardwareTimer pwmtimer2(2);
 HardwareTimer pwmtimer3(3);
 HardwareTimer pwmtimer4(4);
+
+volatile float freqCh1 = 1.0, freqCh2 = 2.0, freqCh3 = 3.0, freqCh4 = 4.0;
 
 
 //ISR's
@@ -66,6 +68,26 @@ void encoderISR()
 void encoderButtonISR()
 {
   encoder.readPushButton();
+}
+
+void setFrequency(HardwareTimer *timer, float frequency) {
+  if (frequency > 0) {
+    int val = int(BASE_PERIOD / frequency);
+    timer->setOverflow(val);
+    //pwmWrite(TIMER1_CH1_PIN, val / 2);
+    if (timer == &pwmtimer1) {
+      pwmWrite(TIMER1_CH1_PIN, val / 2);
+    } else if (timer == &pwmtimer2) {
+      pwmWrite(TIMER2_CH1_PIN, val / 2);
+    } else if (timer == &pwmtimer3) {
+      pwmWrite(TIMER3_CH1_PIN, val / 2);
+    } else if (timer == &pwmtimer4) {
+      pwmWrite(TIMER4_CH1_PIN, val / 2);
+    } else {
+      lcd.setCursor(0, 0);
+      lcd.print("Error!");
+    };
+  }
 }
 
 // the setup function runs once when you press reset or power the board
@@ -82,36 +104,36 @@ void setup() {
   pwmtimer1.pause();
   pwmtimer1.setPrescaleFactor(PRES_FACTOR); //
   pwmtimer1.setOverflow(BASE_PERIOD); //set period
-  pwmWrite(TIMER1_CH1_PIN,BASE_PERIOD/2); //set duty cycle
+  pwmWrite(TIMER1_CH1_PIN, BASE_PERIOD / 2); //set duty cycle
   pwmtimer1.refresh();
   pwmtimer1.resume();
-  
+
   //timer2
   pwmtimer2.pause();
   pwmtimer2.setPrescaleFactor(PRES_FACTOR); //
   pwmtimer2.setOverflow(BASE_PERIOD); //set period
-  pwmWrite(TIMER2_CH1_PIN,BASE_PERIOD/2); //set duty cycle
+  pwmWrite(TIMER2_CH1_PIN, BASE_PERIOD / 2); //set duty cycle
   pwmtimer2.refresh();
   pwmtimer2.resume();
-  
+
   //timer1
   pwmtimer3.pause();
   pwmtimer3.setPrescaleFactor(PRES_FACTOR); //
   pwmtimer3.setOverflow(BASE_PERIOD); //set period
-  pwmWrite(TIMER3_CH1_PIN,BASE_PERIOD/2); //set duty cycle
+  pwmWrite(TIMER3_CH1_PIN, BASE_PERIOD / 2); //set duty cycle
   pwmtimer3.refresh();
   pwmtimer3.resume();
-  
+
   //timer1
   pwmtimer4.pause();
   pwmtimer4.setPrescaleFactor(PRES_FACTOR); //
   pwmtimer4.setOverflow(BASE_PERIOD); //set period
-  pwmWrite(TIMER4_CH1_PIN,BASE_PERIOD/2); //set duty cycle
+  pwmWrite(TIMER4_CH1_PIN, BASE_PERIOD / 2); //set duty cycle
   pwmtimer4.refresh();
   pwmtimer4.resume();
-  
-  
-  
+
+
+
   //encoder
   encoder.begin();                                                           //set encoders pins as input & enable built-in pullup resistors
 
@@ -130,7 +152,7 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  val=int(BASE_PERIOD/encoder.getValue());
+  val = int(BASE_PERIOD / encoder.getValue());
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
@@ -138,10 +160,11 @@ void loop() {
   lcd.print(encoder.getValue());
   lcd.setCursor(10, 1);
   lcd.print(val);
-  pwmtimer1.setOverflow(val);
-  pwmWrite(TIMER1_CH1_PIN,val/2);
-//  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(100);              // wait for a second
-//  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
-//  delay(100);              // wait for a second
+  //  pwmtimer1.setOverflow(val);
+  //  pwmWrite(TIMER1_CH1_PIN, val / 2);
+  setFrequency(&pwmtimer1, 100.0);
+  //  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //  delay(100);              // wait for a second
+  //  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
+  //  delay(100);              // wait for a second
 }
